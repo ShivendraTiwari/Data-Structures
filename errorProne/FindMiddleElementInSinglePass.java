db414f63f8bba0c561160081b8c80b6a9729d6ff
Shivendra Tiwari <60955502+ShivendraTiwari@users.noreply.github.com>
2020-03-07 01:33:20 +0530

    Add files via upload

---
+++ errorProne/FindMiddleElementInSinglePass.java
@@ -0,0 +1,76 @@
+package com.shiv.LinkedList;
+
+class CreateLink {
+	public int dData;
+	public CreateLink next;
+
+	public CreateLink(int data) {
+		this.dData = data;
+	}
+
+	public void displayLink() {
+		System.out.println("Data is:" + dData);
+	}
+}
+
+class FindMiddleElement {
+	private CreateLink first;
+
+	public FindMiddleElement() {
+		first = null;
+	}
+
+	public boolean isEmpty() {
+		return (first == null);
+	}
+
+	public void insertLink(int dd) {
+		CreateLink newLink = new CreateLink(dd);
+		if (isEmpty()) {
+			first = newLink;
+		}
+		first = first.next;
+	}
+
+	public void findMiddle() {
+		CreateLink rabbit = first;
+		CreateLink tortoise = first;
+		if(first!=null) {
+		while ( rabbit!= null && rabbit.next!=null ) {
+
+			rabbit = rabbit.next.next;
+			tortoise = tortoise.next;
+		}
+	System.out.println(tortoise.dData);
+	}}
+
+	public void displayList() {
+		CreateLink current = first;
+		while (current != null) {
+			current.displayLink();
+			current = current.next;
+		}
+	}
+
+}
+
+public class FindMiddleElementInSinglePass {
+
+	public static void main(String[] args) {
+
+		FindMiddleElement findMiddleList = new FindMiddleElement();
+		findMiddleList.insertLink(20);
+		findMiddleList.insertLink(30);
+		findMiddleList.insertLink(60);
+		findMiddleList.insertLink(40);
+		findMiddleList.insertLink(04);
+
+		findMiddleList.displayList();
+
+		findMiddleList.findMiddle();
+		
+		findMiddleList.displayList();
+		
+	}
+
+}
